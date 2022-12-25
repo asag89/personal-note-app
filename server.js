96fac639266ee679e2b7d413fbaff812d92c7ae1
Abdullah Sag <asag8945@gmail.com>
2022-12-25 19:10:44 +0300

    prepare deploy

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ import express from "express"
 import dotenv from "dotenv"
 
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 dotenv.config()
 const app = express()
 
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize';
 
 // error handling package
 import "express-async-errors"
@@ -22,12 +29,25 @@ import noteRouter from "./routes/noteRoutes.js"
 import errorHandlerMiddleware from "./middleware/errorHandler.js"
 import notFoundMiddleware from "./middleware/notFound.js"
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, './client/build')));
 app.use(express.json())
+
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
+
 
 // middleware
 app.use("/api/auth", authRouter)
 app.use("/api/goals", goalRouter)
 app.use("/api/notes", noteRouter)
+
+// only when ready to deploy
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
